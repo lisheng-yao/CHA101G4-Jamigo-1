815ff50ad11bdbc7b2d8d5729ce6ad93ba493333
fudanneko <nuttertools5790@gmail.com>
2023-06-20 00:01:41 +0800

    修改

--- src/main/java/com/jamigo/Application.java
+++ src/main/java/com/jamigo/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @SpringBootApplication
-@ServletComponentScan("com.jamigo.*.*.controller")
 public class Application {
 
     public static void main(String[] args) {
