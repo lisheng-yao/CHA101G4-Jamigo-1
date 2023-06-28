ff118f9aad33c54c11d42f0138efe3999cb5e881
Shawn <crucifix8016@gmail.com>
2023-06-28 14:53:02 +0800

    no message

--- src/main/resources/static/index/頁首頁尾_final/js/script.js
+++ src/main/resources/static/index/頁首頁尾_final/js/script.js
@@ -12,10 +12,47 @@ links.forEach(link => {
 
 //登入登出
 function openmemberinfo(){
-document.getElementById('signin').style.display = 'none';
-document.getElementById('memberinfo').style.display = 'flex';
+    document.getElementById('signin').style.display = 'none';
+    document.getElementById('memberinfo').style.display = 'flex';
 }
 function opensignin(){
-document.getElementById('signin').style.display = 'flex';
-document.getElementById('memberinfo').style.display = 'none';
+    document.getElementById('signin').style.display = 'flex';
+    document.getElementById('memberinfo').style.display = 'none';
+
+}
+( ()=> {
+    const memberimg=document.querySelector('#memberimg');
+    const membername=document.querySelector('#membername');
+    const memberid = localStorage.getItem('memberNo');
+    const memberaccount = localStorage.getItem('memberAccount');
+    checklogin();//確認登入方法
+
+    function checklogin(){
+        if(memberid){
+            openmemberinfo();
+            memberimg.src= `/Jamigo/member/member_data/${memberid}`;
+            membername.innerText='HI!  ' +  memberaccount;
+        }
+    }
+
+    const logout=document.querySelector('#logoutbutton');
+    logout.addEventListener('click',()=>{
+        opensignin();
+        localStorage.setItem('memberNo','');
+        localStorage.setItem('memberAccount','');
+        window.location.href = "/Jamigo/index/首頁/index.html";
+    })
+
+})();
+
+function check(event) {
+
+    const memberNo = localStorage.getItem('memberNo');
+    if (memberNo) {
+        return;
+    } else {
+        event.preventDefault();
+        window.location.href = '/Jamigo/member/login/login.html';
+    }
+
 }
