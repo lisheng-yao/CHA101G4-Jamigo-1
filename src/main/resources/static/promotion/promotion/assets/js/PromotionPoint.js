086f0365b1c8301690e943aa73b07967e49b354d
fudanneko <nuttertools5790@gmail.com>
2023-06-29 18:10:44 +0800

    施工

--- src/main/resources/static/promotion/promotion/assets/js/PromotionPoint.js
+++ src/main/resources/static/promotion/promotion/assets/js/PromotionPoint.js
@@ -77,7 +77,7 @@
                 <div class="modal-dialog">
                     <div class="modal-content">
                         <div class="modal-header">
-                            <h1 class="modal-title fs-5" id="exampleModalLabel${i}">折價券活動修改
+                            <h1 class="modal-title fs-5" id="exampleModalLabel${i}">點數活動修改
                             </h1>
                             <button type="button" class="btn-close" data-bs-dismiss="modal"
                                     aria-label="Close"></button>
