58fae893d6c01c141ce0feb5c3d596a8601fe1e4
COCO <ssrichi171@gmail.com>
2023-06-30 20:45:52 +0800

    update adminLogin & delet activityWebSocket

--- src/main/java/com/jamigo/AdminLoginInterceptor.java
+++ src/main/java/com/jamigo/AdminLoginInterceptor.java
@@ -1,5 +1,7 @@
 package com.jamigo;
 
+import java.util.Enumeration;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -13,28 +15,32 @@ public class AdminLoginInterceptor implements HandlerInterceptor {
 	@Override
     public boolean preHandle(HttpServletRequest req, HttpServletResponse resp, Object handler)
             throws Exception {
-		Object attribute = req.getSession().getAttribute("user");
-		if(attribute == null) {
+		Enumeration<String> aaa = req.getSession().getAttributeNames();
+		while (aaa.hasMoreElements()) {
+			System.out.println(aaa.nextElement());
+		}
+		System.out.println("session id 為" + req.getSession().getId());
+		if(req.getSession().getAttribute("counter") == null) {
+			System.out.println("session中的platform為null");
 			// 在進入處理器方法之前執行的邏輯
 			// 此處可以將當前頁面的 URL 存儲到會話中
 //			String currentUrl = req.getRequestURL().toString();
 //			req.getSession().setAttribute("currentUrl", currentUrl);
 			resp.sendRedirect("/Jamigo/platform/login/admin_login.html");
 			return false;
-		} else {
-			return true; // 返回 true 表示繼續執行後續的處理器和攔截器			
 		}
+		
+		System.out.println("session中的platform為true");
+		return true; // 返回 true 表示繼續執行後續的處理器和攔截器			
     }
 
     @Override
     public void postHandle(HttpServletRequest req, HttpServletResponse resp, Object handler,
             ModelAndView modelAndView) throws Exception {
-        // 在執行完處理器方法之後，返回 ModelAndView 之前執行的邏輯
     }
 
     @Override
     public void afterCompletion(HttpServletRequest req, HttpServletResponse resp, Object handler,
             Exception ex) throws Exception {
-        // 在整個請求完成之後執行的邏輯
     }
 }
