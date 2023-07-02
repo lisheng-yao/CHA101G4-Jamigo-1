6517bad87475bbe22f2e7a068bc9c231eb58ddde
COCO <ssrichi171@gmail.com>
2023-07-02 11:12:44 +0800

    update admin login / logout & carouselInsert / activityOrder / admin login js

--- src/main/java/com/jamigo/counter/activityOrder/entity/ActivityOrderService.java
+++ src/main/java/com/jamigo/counter/activityOrder/entity/ActivityOrderService.java
@@ -1,10 +1,20 @@
 package com.jamigo.counter.activityOrder.entity;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
+import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.jamigo.shop.platform_order.entity.PlatformOrder;
+
+import ecpay.payment.integration.AllInOne;
+import ecpay.payment.integration.domain.AioCheckOutALL;
 
 
 @Service
@@ -35,4 +45,12 @@ public class ActivityOrderService {
 		return repository.findAll();
 	}
 	
+	public void changePaidStat(ActivityOrderVO activityOrderVO) {
+		
+		System.out.println("執行更新setActivityPaymentStat");
+		
+		activityOrderVO.setActivityPaymentStat(Byte.valueOf((byte) 1));
+		
+		System.out.println("PaymentStat" + activityOrderVO.getActivityPaymentStat());
+    }
 }
