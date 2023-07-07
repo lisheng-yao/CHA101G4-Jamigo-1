c98d16db8925d9487da8c8e4c088d37ccd6e2c48
Kodey Wu <helpful.shibe.2655@gmail.com>
2023-07-07 13:54:50 +0800

    排程器

--- src/main/java/com/jamigo/SpringAsyncConfig.java
+++ src/main/java/com/jamigo/SpringAsyncConfig.java
@@ -3,11 +3,13 @@ package com.jamigo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.concurrent.Executor;
 
 @Configuration
+@EnableScheduling
 @EnableAsync
 public class SpringAsyncConfig {
 
